3f1b3a022fb544f00ea2dd16061f47fdf9acef0b
Mdigennaro <michele.digennaro719@gmail.com>
2022-01-18 17:19:36 +0100

    fine classe product

--- classes/Product.php
+++ classes/Product.php
@@ -4,19 +4,62 @@ class Product{
 
   private $name;
   private $brand;
+  private $size;
   private $price;
   private $discount = 0;
-  private $size;
 
   public function __construct($_name, $_brand)
   {
-    $this->name = $_name;
-    $this->brand = $_brand;
+    $this -> name = $_name;
+    $this -> brand = $_brand;
   }
 
   public function setName($_name){
-    $this->name = $_name;
+    $this -> name = $_name;
   }
+
+  public function setBrand($_brand){
+    $this -> brand = $_brand;
+  }
+  
+  public function setSize($_size){
+    $this -> size = $_size; 
+  }
+
+  public function setPrice($_price){
+    $this -> price = $_price;
+  }
+
+  public function setDiscount($_discount){
+    $this -> discount = $_discount;
+  }
+
+  public function getName(){
+    return $this -> name;
+  }
+
+  public function getBrand(){
+    return $this -> brand;
+  }
+  
+  public function getSize(){
+    return $this -> size;
+  }
+  
+  public function getPrice(){
+    return $this -> price;
+  }
+
+  public function getDiscount(){
+    return $this -> discount;
+  }
+
+  public function getFinalPrice(){
+    $finalPrice = $this -> price - (($this -> price * $this -> discount) / 100);
+
+    return number_format($finalPrice, 2, ",", "");
+  }
+
 
 }
 
--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 require_once __DIR__."/classes/Product.php";
 
 $new_product = new Product("Giubbotto", "MDG");
+$new_product -> setPrice(200);
+$new_product -> setSize("XXL");
+$new_product -> setDiscount(20);
 
 var_dump($new_product)
 
@@ -18,6 +21,24 @@ var_dump($new_product)
   <title>Shop</title>
 </head>
 <body>
+
+<h1>BOOLAZON</h1>
+
+<h2> <?php echo $new_product -> getName() ?> </h2>
+
+<h3> <?php echo $new_product -> getBrand() ?> </h3>
+
+<h5> <?php echo $new_product -> getSize() ?> </h5>
+
+<p>
+  Prezzo: <?php echo $new_product -> getPrice() ?> €  
+</p>
+<p>
+  Sconto: <?php echo $new_product -> getDiscount() ?> %  
+</p>
+<p>
+  Prezzo finale: <?php echo $new_product -> getFinalPrice() ?> €
+  </p>
   
 </body>
 </html>
